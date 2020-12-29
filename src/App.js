57851de2efb31d96085cff8d6bbb6e2965f1d584
gunnmgirl <gunnmgirl@gmail.com>
2020-12-29 10:13:01 +0100

    Adds page navigation with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import React from "react";
+import { Router, Switch, Route } from "react-router-dom";
+
+import history from "./routing/history";
+import LandingPage from "./features/landing/components/LandingPage";
+import Contact from "./features/contact/components/ContactPage";
+import About from "./features/about/components/AboutPage";
+import PageNotFound from "./components/PageNotFound";
+import Apply from "./features/application/components/Apply";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/apply" component={Apply} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
