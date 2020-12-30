45398163ce9a7add10f2ac17946c7d44c43489cf
gunnmgirl <gunnmgirl@gmail.com>
2020-12-30 09:53:31 +0100

    DEV-10 Implements Landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Router, Switch, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
 import history from "./routing/history";
 import LandingPage from "./features/landing/components/LandingPage";
@@ -7,18 +8,23 @@ import Contact from "./features/contact/components/ContactPage";
 import About from "./features/about/components/AboutPage";
 import PageNotFound from "./components/PageNotFound";
 import Apply from "./features/application/components/Apply";
+import GlobalStyle from "./GlobalStyle";
+import defaultTheme from "./themes/defaultTheme";
 
 function App() {
   return (
-    <Router history={history}>
-      <Switch>
-        <Route path="/" exact component={LandingPage} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/apply" component={Apply} />
-        <Route component={PageNotFound} />
-      </Switch>
-    </Router>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <Router history={history}>
+        <Switch>
+          <Route path="/" exact component={LandingPage} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/apply" component={Apply} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ src/themes/defaultTheme.js
@@ -0,0 +1,10 @@
+const defaultTheme = {
+  primary: "#ffffff",
+  onPrimary: "#000000",
+  secondary: "#ff721c",
+  onSecondary: "#ffffff",
+  onActive: "#ff721c",
+  onHover: "#ff6100",
+};
+
+export default defaultTheme;
